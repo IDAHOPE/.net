fc46a0c22ce19fcc93c7a3d21359ae04f1b16a58
IDAHOPE <105875871+IDAHOPE@users.noreply.github.com>
2023-05-25 17:22:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using API.Data;
+using API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddDbContext<RoomDbContext>(
+    o => o.UseSqlite("Data Source=rooms.db"));
+
+builder.Services.AddCors(p => p.AddPolicy("corspolicy",build =>
+{
+    build.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("corspolicy");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
