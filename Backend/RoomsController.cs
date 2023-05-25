5d6c5e03a89cf92aa8a482032908b8d950454306
IDAHOPE <105875871+IDAHOPE@users.noreply.github.com>
2023-05-25 17:27:34 +0530

    Add files via upload

---
+++ Backend/RoomsController.cs
@@ -0,0 +1,61 @@
+﻿using API.Data;
+using API.Models;
+using API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly IRoomRepository _roomRepository;
+        public RoomsController(IRoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Room>> GetRooms()
+        {
+            return await _roomRepository.Get();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Room>> GetRooms(int id)
+        {
+            return await _roomRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Room>> PostRooms([FromBody] Room room)
+        {
+            var newRoom = await _roomRepository.Create(room);
+            return CreatedAtAction(nameof(GetRooms), new { Id = newRoom.RoomId }, newRoom);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutRooms(int id, [FromBody] Room room)
+        {
+            if (id != room.RoomId)
+            {
+                return BadRequest();
+            }
+            await _roomRepository.Update(room);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var roomToDelete = await _roomRepository.Get(id);
+            if (roomToDelete == null)
+                return NotFound();
+
+            await _roomRepository.Delete(roomToDelete.RoomId);
+            return NoContent();
+        }
+    }
+}
